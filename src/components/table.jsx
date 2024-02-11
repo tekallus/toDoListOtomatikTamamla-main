a95b5188c6742170aeda28562caf7ba386fbaa6c
“tekallus <dorukkuheylan@gmail.com>
2024-02-12 01:48:41 +0300

    change1

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -9,19 +9,35 @@ const FlightGraphs = () => {
         borderRadius: '5px',
         padding: '20px',
         boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        backgroundColor: '#f10f10f10',
+        borderRadius: '20px',
       }}
     >
-      <div className="header" style={{ display: 'flex' }}>
-        <h2>Flight Graphs</h2>
-      </div>
       <div
-        className="window-buttons"
-        style={{ display: 'flex', flexDirection: 'row-reverse' }}
+        className="header"
+        style={{ display: 'flex', justifyContent: 'space-between' }}
       >
-        <button className="window-button close">&#x2716;</button>
-        <button className="window-button minimize">&#x2501;</button>
-        <button className="window-button maximize">&#x2751;</button>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img
+            src="windows_icon.png"
+            alt="Windows Icon"
+            style={{ marginRight: '8px', width: '24px', height: '24px' }}
+          />
+
+          <h2>Flight Graphs</h2>
+        </div>
+        <div
+          className="window-buttons"
+          style={{ display: 'flex', flexDirection: 'row-reverse' }}
+        >
+          <button className="window-button close">&#x2716;</button>
+          <div style={{ marginRight: '40px' }}></div>
+          <button className="window-button maximize">&#x2751;</button>
+          <div style={{ marginRight: '40px' }}></div>
+          <button className="window-button minimize">&#x2501;</button>
+        </div>
       </div>
+
       <table style={{ width: '100%', borderCollapse: 'collapse' }}>
         <tbody>
           <tr>
@@ -92,36 +108,50 @@ const FlightGraphs = () => {
                 width: '50%',
                 padding: '10px',
                 borderBottom: '1px solid #ccc',
+                border: '2px solid black',
               }}
             >
               <div
                 className="flight-graphs-table"
-                style={{ overflow: 'auto', maxHeight: '300px' }}
+                style={{
+                  overflow: 'auto',
+                  maxHeight: '300px',
+                }}
               >
-                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                <table
+                  style={{
+                    width: '100%',
+                    borderCollapse: 'collapse',
+                    padding: '5px',
+                    borderBottom: '1px solid #ccc',
+                  }}
+                >
                   <thead>
                     <tr>
                       <th
                         style={{
                           padding: '5px',
                           borderBottom: '1px solid #ccc',
+                          borderRight: '1px solid #ccc',
                         }}
                       ></th>
                       <th
                         style={{
                           padding: '5px',
                           borderBottom: '1px solid #ccc',
+                          borderRight: '1px solid #ccc',
                         }}
                       >
-                        Item
+                        <h4 style={{ margin: '0' }}>Item</h4>
                       </th>
                       <th
                         style={{
                           padding: '5px',
                           borderBottom: '1px solid #ccc',
+                          borderRight: '1px solid #ccc',
                         }}
                       >
-                        Category
+                        <h4 style={{ margin: '0' }}>Category</h4>
                       </th>
                     </tr>
                   </thead>
@@ -129,13 +159,32 @@ const FlightGraphs = () => {
                     {[...Array(20)].map((_, index) => (
                       <tr
                         key={index}
-                        style={{ borderBottom: '1px solid #ccc' }}
+                        style={{
+                          borderBottom: '1px solid #ccc',
+                          borderRight: '1px solid #ccc',
+                        }}
                       >
-                        <td style={{ padding: '5px' }}>
+                        <td
+                          style={{
+                            padding: '5px',
+                            textAlign: 'left',
+                            borderRight: '1px solid #ccc',
+                          }}
+                        >
                           <input type="checkbox" />
                         </td>
-                        <td style={{ padding: '5px' }}>aileron Input</td>
-                        <td style={{ padding: '5px' }}>RCServoInf</td>
+                        <td
+                          style={{
+                            padding: '5px',
+                            textAlign: 'left',
+                            borderRight: '1px solid #ccc',
+                          }}
+                        >
+                          aileron Input
+                        </td>
+                        <td style={{ padding: '5px', textAlign: 'left' }}>
+                          RCServoInf
+                        </td>
                       </tr>
                     ))}
                   </tbody>
@@ -148,11 +197,15 @@ const FlightGraphs = () => {
                 padding: '10px',
                 verticalAlign: 'top',
                 borderBottom: '1px solid #ccc',
+                border: '2px solid #808080',
               }}
             >
               <div
                 className="empty-screen-div"
-                style={{ height: '200px', overflow: 'auto' }}
+                style={{
+                  height: '300px',
+                  overflow: 'auto',
+                }}
               ></div>
             </td>
           </tr>
